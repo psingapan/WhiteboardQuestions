9b126412b78675751d74b17b8401c89800c8a57c
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-08-23 10:07:52 -0700

    Add files via upload
    Given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
    Return the shuffled string.
    
    Example 1:
    int[] indices= {4,5,6,7,0,2,1,3}
    string= "codeleet"
    
    output: string= "leetcode"

---
+++ ShuffleString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace ShuffleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //driver code
+            string ss = "Hello";
+            int[] array1 = new int[] { 3, 1, 2, 0, 4 };
+            RestoreString(ss, array1);
+            
+        }
+            public static void RestoreString(string s, int[] indices)
+            {
+                //initialize a new character array to store characters of string
+                char[] charArray=new char[indices.Length];
+
+                for (int i = 0; i < indices.Length; i++)
+                {
+
+                //the new charArray at index position[i] of indices is = to string position[i]
+                charArray[indices[i]] = s[i];
+                }
+            Console.WriteLine(new string(charArray));
+            }
+        
+    }
+}
