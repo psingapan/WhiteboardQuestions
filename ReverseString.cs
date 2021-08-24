c6568e6b6e829178d9af0030b194285c89ccc540
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-08-23 21:07:45 -0700

    Add files via upload
    Write a function that reverses a string. The input string is given as an array of characters s. 
    
    Example 1:
    Input: s = ["h","e","l","l","o"]
    Output: ["o","l","l","e","h"]
    
    Example 2:
    Input: s = ["H","a","n","n","a","h"]
    Output: ["h","a","n","n","a","H"]
     
    Constraints:
    1 <= s.length <= 105
    s[i] is a printable ascii character.
     
    Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.

---
+++ ReverseString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = new char[] {'a', 'b', 'c', 'd', 'e'};
+            ReverseString(charArray);
+        }
+
+        public static void ReverseString(char[] s)
+        {
+
+            //reversing an array in place requires a starting point, an ending point, and a temp variable used to store values during swaps.
+            int start = 0;
+            int end = s.Length - 1;
+            char temp;
+
+            while (start < end)
+            {
+                // swapping the characters between start and ending values. Starting index increments as ending index
+                // decrements until all values are swapped.
+                temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+                start++;
+                end--;
+
+            }
+
+
+            
+        }
+    }
+
+}
