b7b32bba7ce0a39f4721be7e6e6d47e9ae365d9c
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-09-03 22:37:18 -0700

    Add files via upload
    Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
    
    Example 1:
    Input: s = "A man, a plan, a canal: Panama"
    Output: true
    Explanation: "amanaplanacanalpanama" is a palindrome.
    
    Example 2:
    Input: s = "race a car"
    Output: false
    Explanation: "raceacar" is not a palindrome.

---
+++ ValidPalindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "race a car";
+            IsPalindrome(s);
+        }
+        public static Boolean IsPalindrome(string s)
+        {
+            //transform to character array. Remove all special characters and spaces
+           
+            s=s.ToLower();
+            string b = "";
+    
+            //copy only letters and digit characters from string s onto string b.
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                if (char.IsLetterOrDigit(s[i]))
+                    b += s[i];
+            }
+
+            //check if each element from both ends are equivalent to each other.
+            for (int i = 0, j=b.Length-1; i < b.Length; i++, j--)
+            {
+                if (b[i] != b[j])
+                {
+                    Console.WriteLine(false);
+                    return false;
+                }
+            }
+            Console.WriteLine(true);
+            return true;
+            
+
+        }
+        
+    }
+    
+}
