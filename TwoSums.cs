706d203a8b7f69d2f1737fabbc46d718c4a5f7c0
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-09-08 09:03:35 -0700

    Add files via upload
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
    
     
    Example 1:
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
    
    Example 2:
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    
    Example 3:
    Input: nums = [3,3], target = 6
    Output: [0,1]
     
    
    Constraints:
    2 <= nums.length <= 104
    -109 <= nums[i] <= 109
    -109 <= target <= 109
    Only one valid answer exists.

---
+++ TwoSums.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 11, 7, 2, 15 };
+            int target = 9;
+            TwoSum(nums, target);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            //Time & space complexity is O(n).
+
+            //Use a dictionary to append and track the current index position for each time we pass through an element. 
+            Dictionary<int, int> mydic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //cur is a marker to keep track of the current value that we pass over. We are looking for x which
+                //equates to the target minus the current value of nums[i].
+                int current = nums[i];
+                int x = target - current;
+
+                //search through the dictionary for the key of (x) which is the value of nums[i] and (value) i= index.
+                //if not found, then we append the current value of nums[i] as the key, and value of i as index position.
+                if(mydic.ContainsKey(x))
+                {
+                    Console.WriteLine("{0}, {1}", mydic[x], i );
+                    return new int[] { mydic[x], i };
+                }
+                    
+                mydic.Add(current, i);
+            }
+            return null;
+        }
+    }
+}
