40c622108e09e3cba30ee8ddce4d3aa39249889d
Pirin Singapan <72480602+psingapan@users.noreply.github.com>
2021-09-09 14:00:14 -0700

    Add files via upload
    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue. We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively. You must solve this problem without using the library's sort function.
    
     Example 1:
    Input: nums = [2,0,2,1,1,0]
    Output: [0,0,1,1,2,2]
    
    Example 2:
    Input: nums = [2,0,1]
    Output: [0,1,2]
    
    Example 3:
    Input: nums = [0]
    Output: [0]
    
    Example 4:
    Input: nums = [1]
    Output: [1]
     
    Constraints:
    n == nums.length
    1 <= n <= 300
    nums[i] is 0, 1, or 2.

---
+++ SortColors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            int n = nums.Length;
+            int temp = 0;
+
+
+            //nested forloop to traverse through the array twice to sort items that are smaller to the left and larger
+            //to the right
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    //swapping the smaller with larger element in case that the larger is on the left.
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+        }
+    }
